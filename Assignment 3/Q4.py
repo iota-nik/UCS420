910868dffd1173f22aedd198df8249fa8e11393a
iota-nik <183414930+iota-nik@users.noreply.github.com>
2026-09-18 09:22:22 +0530

    WHole work

--- Assignment 3/Q4.py
+++
@@ -1,8 +0,0 @@
-"""
-Q) Read a csv file and display its first five rows.  Note-: Download dataset from https://www.kaggle.com/datasets/uciml/iris)  
-"""
-
-import pandas as pd
-
-dfCSV = pd.read_csv('Iris.csv')
-print(dfCSV.head())
